ff318228b746e86a1fc818e0816ff6a46d99366e
Service Worker <serviceworker@Supers-MacBook-Pro.local>
2023-06-08 17:08:51 -0400

    test not working yet

--- test/swap-test.js
+++ test/swap-test.js
@@ -2,9 +2,10 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("FKToken", function() {
-	const TOKEN_CAP= 100000000;
+	const TOKEN_CAP= 100000000000000;
 	const TOKEN_BLOCK_REWARD = 500;
 	let owner;
+	let addr1;
 	let FKToken;
 	let fkToken;
 	let LuciaToken;
@@ -13,22 +14,75 @@ describe("FKToken", function() {
 	beforeEach(async function() {
 		[owner, addr1] = await ethers.getSigners();
 		FKToken = await ethers.getContractFactory("FKToken");
-		fkToken = await FKToken.deploy(100000000,TOKEN_BLOCK_REWARD);
+		fkToken = await FKToken.deploy(TOKEN_CAP,TOKEN_BLOCK_REWARD);
 
 		LuciaToken = await ethers.getContractFactory("LuciaToken");
-		luciToken = await LuciaToken.deploy(100000000,TOKEN_BLOCK_REWARD)
+		luciToken = await LuciaToken.deploy(TOKEN_CAP,TOKEN_BLOCK_REWARD)
 		await fkToken.deployed();
 		await luciToken.deployed();
 	})
 
-	it("Shold create 2 new token contracts for FKT and LUCI", async function () {
+	it("Should create 2 new token contracts for FKT and LUCI", async function () {
 		expect(await fkToken.name()).to.equal("FKToken");
 		expect(await fkToken.symbol()).to.equal("FKT");
 		expect(await fkToken.owner()).to.equal(owner.address);
 		await luciToken.transfer(addr1.address,50000000)
 		expect(await luciToken.balanceOf(addr1.address)).to.equal(50000000)
+		expect(await luciToken.owner()).to.equal(owner.address);
+	})
+	
+	it("Should approve transactions and see in allownace", async function () {
+		[owner, addr1, addr2] = await ethers.getSigners();
+		await luciToken.transfer(addr1.address,50000000)
+		TokenSwap = await ethers.getContractFactory("TokenSwap");
+		tokenSwap = await TokenSwap.deploy(
+			fkToken.address,
+			addr1.address,
+			222, 
+			luciToken.address,
+			addr2.address,
+			111  
+		);
+		await tokenSwap.deployed()
 
+		// interesting things happen here
+		await fkToken.transfer(addr1.address,22200000000000)
+		await luciToken.transfer(addr2.address,11100000000000)
+		expect(await fkToken.balanceOf(addr1.address)).to.equal(22200000000000)
+		expect(await luciToken.balanceOf(addr2.address)).to.equal(11100000000000)
+
+		await luciToken.connect(addr1).approve(tokenSwap.address, 22200000000000);
+		await fkToken.connect(addr2).approve(tokenSwap.address, 11100000000000);
+		let allowedTokensSCLuci = await luciToken.allowance(addr1.address, tokenSwap.address);
+		let allowedTokensSCFKT = await fkToken.allowance(addr2.address, tokenSwap.address);
+		expect(ethers.utils.formatEther(allowedTokensSCLuci)).to.equal('0.0000222');
+		expect(ethers.utils.formatEther(allowedTokensSCFKT)).to.equal('0.0000111');
+
+		console.log("allowedTokensSCLuci: ",allowedTokensSCLuci);
+		console.log("allowedTokensSCFKT: ",allowedTokensSCFKT);
+		// end interesting things
 		
+		await tokenSwap.connect(addr1).swap()
+
+
+		let bal1 = await luciToken.balanceOf(addr2.address)
+		let bal2 = await fkToken.balanceOf(addr1.address)
+		console.log("bal1, bal2: ",bal1, bal2)
+		// // console.log("tokenSwap: ",tokenSwap);
+
+		// /**
+		// address _token1,
+        // address _owner1,
+        // uint _amount1,
+        // address _token2,
+        // address _owner2,
+        // uint _amount2
+        // */
+
+        // await tokenSwap.swap()
+        // expect(await luciToken.balanceOf(owner.address)).to.equal(111);
+        // expect(await fkToken.balanceOf(addr1.address)).to.equal(222);
+
 	})
 
 })
