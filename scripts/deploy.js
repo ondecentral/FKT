776e9b5d0a5fcdf2673aef42a59247b21efd55f1
romesca <rome@blades.network>
2023-06-14 16:04:58 -0400

    contract creation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,6 +8,7 @@ const hre = require("hardhat");
 
 async function main() {
   let [owner, addr1, addr2] = await ethers.getSigners();
+  console.log("[owner, addr1, addr2]: ",[owner, addr1, addr2]);
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
@@ -18,11 +19,11 @@ async function main() {
 
   const TOKEN_CAP= 100000000;
   const TOKEN_BLOCK_REWARD = 500;
-  const FKToken = hre.ethers.getContractFactory("FKToken");
+  const FKToken = await hre.ethers.getContractFactory("FKToken");
   const fkToken = await FKToken.deploy(TOKEN_CAP,TOKEN_BLOCK_REWARD)
   await fkToken.deployed();
 
-  const LuciaToken = hre.ethers.getContractFactory("LuciaToken");
+  const LuciaToken = await hre.ethers.getContractFactory("LuciaToken");
   const luciToken = await LuciaToken.deploy(TOKEN_CAP,TOKEN_BLOCK_REWARD);
   await luciToken.deployed();
 
