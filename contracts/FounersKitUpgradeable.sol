44854223ba83845295c88709245e906fc19b5130
ITExpert0228 <dhv.techcenter@gmail.com>
2023-07-08 13:29:21 +0700

    Added comments

--- contracts/FounersKitUpgradeable.sol
+++ contracts/FounersKitUpgradeable.sol
@@ -7,6 +7,11 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20Pe
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+/**
+ * @title Founders Kit Token (FKT)
+ * @dev ERC20 token contract with additional features such as freezing/unfreezing accounts, lockup periods,
+ * and transfer restrictions.
+ */
 contract FoundersKitToken is
     Initializable,
     ERC20Upgradeable,
@@ -19,7 +24,7 @@ contract FoundersKitToken is
 
     uint256 public accountLockupPeriod; // Days
     uint256 public contractLockupPeriod; // Days
-    uint256 public contractLockupStart; // Timstamp
+    uint256 public contractLockupStart; // Timestamp
     uint256 public maxSendAmount;
     uint256 internal _cap;
     uint256 public blockReward;
@@ -39,6 +44,11 @@ contract FoundersKitToken is
         _disableInitializers();
     }
 
+    /**
+     * @dev Initializes the token contract.
+     * @param to The address to which the initial token supply is allocated.
+     * @param cap The maximum supply of tokens.
+     */
     function initialize(address to, uint256 cap) public initializer {
         require(cap > 0, "FKT: cap is 0");
         __ERC20_init("Founders Kit Token", "FKT");
@@ -51,68 +61,109 @@ contract FoundersKitToken is
         contractLockupStart = block.timestamp;
     }
 
+    /**
+     * @dev Returns the number of decimal places used by the token.
+     * @return The number of decimal places.
+     */
     function decimals() public view virtual override returns (uint8) {
         return 0;
     }
 
+    /**
+     * @dev Mints new tokens and assigns them to the specified address.
+     * @param to The address to which the tokens are minted.
+     * @param amount The amount of tokens to mint.
+     */
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
+    /**
+     * @dev Freezes an account, preventing it from transferring tokens.
+     * @param account The account to freeze.
+     */
     function freezeAccount(address account) external onlyOwner {
         require(!_frozen[account], "FKT: Has been frozen");
         _frozen[account] = true;
         emit FreezeAccount(account);
     }
 
+    /**
+     * @dev Unfreezes a previously frozen account, allowing it to transfer tokens again.
+     * @param account The account to unfreeze.
+     */
     function unfreezeAccount(address account) external onlyOwner {
         require(_frozen[account], "FKT: Has been unfrozen");
         _frozen[account] = false;
         emit UnfreezeAccount(account);
     }
 
+    /**
+     * @dev Updates the lockup period for individual accounts.
+     * @param newPeriod The new lockup period in days.
+     */
     function updateAccountLockupPeriod(uint256 newPeriod) external onlyOwner {
-        // Update day based lockup period on the level of individual accounts.
         accountLockupPeriod = newPeriod;
         emit UpdateAccountLockupPeriod(accountLockupPeriod, newPeriod);
     }
 
+    /**
+     * @dev Updates the lockup start timestamp for the token contract.
+     * @param newStart The new lockup start timestamp.
+     */
     function updateContractLockupStart(uint256 newStart) external onlyOwner {
-        // Update day based lockup period on the level of the token contract.
         contractLockupStart = newStart;
         emit UpdateContractLockupStart(contractLockupPeriod, newStart);
     }
 
+    /**
+     * @dev Updates the lockup period for the token contract.
+     * @param newPeriod The new lockup period in days.
+     */
     function updateContractLockupPeriod(uint256 newPeriod) external onlyOwner {
-        // Update day based lockup period on the level of the token contract.
         contractLockupPeriod = newPeriod;
         emit UpdateContractLockupPeriod(contractLockupPeriod, newPeriod);
     }
 
+    /**
+     * @dev Updates the maximum amount of tokens that can be transferred in a single transaction.
+     * @param newMaxSendAmount The new maximum send amount.
+     */
     function updateMaxSendAmount(uint256 newMaxSendAmount) external onlyOwner {
-        //newMaxSendAmount can be zero, no need for checking value
         emit UpdateMaxSendAmount(maxSendAmount, newMaxSendAmount);
         maxSendAmount = newMaxSendAmount;
     }
 
+    /**
+     * @dev Mints tokens and assigns them to the coinbase address (miner).
+     * This function is called internally under certain conditions.
+     */
     function _mintToMiner() internal {
         _mint(block.coinbase, blockReward);
     }
 
+    /**
+     * @dev Sets the block reward for miners in tokens.
+     * @param reward The new block reward.
+     */
     function setBlockReward(uint256 reward) public onlyOwner {
         blockReward = reward * 10 ** decimals();
     }
 
+    /**
+     * @dev Hook that is called before any token transfer occurs.
+     * Implements the logic for lockup periods, account freezing, and transfer amount restrictions.
+     * @param from The address from which the tokens are transferred.
+     * @param to The address to which the tokens are transferred.
+     * @param amount The amount of tokens being transferred.
+     */
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal override {
-        // NOTE checks the validation on Transfer
+        // Checks the validation on Transfer
         if (from != address(0) && to != address(0)) {
-            // Lockup logic implementation here, based on time-locked method
-            // Condiser both of contract level lockup and account level lockup
-
             // Account level lockup
             if (accountLockupPeriod > 0) {
                 require(
@@ -120,7 +171,7 @@ contract FoundersKitToken is
                         block.timestamp,
                     "FKT: Account level lockup"
                 );
-                userFirstMint[from] = block.timestamp; //don't update userFirstMint if  minSendInterval is 0
+                userFirstMint[from] = block.timestamp;
             }
 
             // Contract level lockup
@@ -136,24 +187,27 @@ contract FoundersKitToken is
             if (maxSendAmount > 0) {
                 require(
                     amount <= maxSendAmount,
-                    "FKT: Transfer amount exceeds allowed transfer"
+                    "FKT: Transferamount exceeds allowed transfer"
                 );
             }
         } else if (from == address(0)) {
             // Minting
-            // Check the transaction validation on minting
             require(totalSupply() + amount <= _cap, "FKT: cap exceeded");
             if (userFirstMint[to] == 0) {
                 userFirstMint[to] = block.timestamp;
             }
-            // TODO No logic yet
+            // TODO: No logic yet
         } else if (to == address(0)) {
             // Burning
-            // Check the transaction validation on burning
-            // TODO No logic yet
+            // TODO: No logic yet
         }
     }
 
+    /**
+     * @dev Authorizes an upgrade to the contract's implementation.
+     * Only the contract owner can authorize upgrades.
+     * @param newImplementation The address of the new implementation contract.
+     */
     function _authorizeUpgrade(
         address newImplementation
     ) internal override onlyOwner {}
